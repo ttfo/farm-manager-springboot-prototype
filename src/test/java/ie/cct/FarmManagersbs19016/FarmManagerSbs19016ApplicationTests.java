6554d5dfbc75711c382515c0083411ae4d113ad9
ttfo <puntogif@gmail.com>
2020-05-14 18:07:48 +0100

    Initial commit

---
+++ src/test/java/ie/cct/FarmManagersbs19016/FarmManagerSbs19016ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.cct.FarmManagersbs19016;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmManagerSbs19016ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
